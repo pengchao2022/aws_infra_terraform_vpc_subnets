30bc29554043afef8971c77c05da1d0d10d42ca1
pengchao2022 <pengchao.ma2@outlook.com>
2026-05-31 14:28:05 +0800

    feat: GTP-54273 added the backend code

---
+++ terraform/backend.tf
@@ -0,0 +1,16 @@
+# infra-vpc/terraform/backend.tf
+terraform {
+  backend "s3" {
+
+    bucket = "pengchao2022-terraform-state"
+
+    key = "vpc/terraform.tfstate"
+    
+    
+    region = "us-east-1"
+    
+    dynamodb_table = "terraform-state-lock"
+
+    encrypt = true
+  }
+}
