9be2b154768090ab73221ef6d98a72a124ce613d
pengchao2022 <pengchao.ma2@outlook.com>
2026-06-05 11:27:47 +0800

    feat: GTP-54273 modified the code

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -22,3 +22,8 @@ output "public_route_table_id" {
   description = "ID of the public route table"
   value       = aws_route_table.public.id
 }
+
+output "private_route_table_ids" {
+  description = "List of IDs for the private route tables"
+  value       = aws_route_table.private[*].id
+}
